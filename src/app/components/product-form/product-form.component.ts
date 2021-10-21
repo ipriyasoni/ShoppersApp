8a0638aa379800c8c89cdeb829c5682ab6b846d7
Priya Soni <creativepriya10@gmail.com>
2021-10-21 15:53:52 +0530

    modals fix

--- src/app/components/product-form/product-form.component.ts
+++ src/app/components/product-form/product-form.component.ts
@@ -1,48 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductService } from 'src/app/services/product.service';
-import { Router, ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { Product } from 'src/app/models/Product';
+import { Component, OnInit } from "@angular/core";
+import { ProductService } from "src/app/services/product.service";
+import { Router, ActivatedRoute } from "@angular/router";
+import { Observable } from "rxjs";
+import { Product } from "src/app/models/Product";
 
 @Component({
-  selector: 'app-product-form',
-  templateUrl: './product-form.component.html',
-  styleUrls: ['./product-form.component.css']
+  selector: "app-product-form",
+  templateUrl: "./product-form.component.html",
+  styleUrls: ["./product-form.component.css"],
 })
 export class ProductFormComponent implements OnInit {
- product: Product = {
-   title: null,
-   imageUrl: null,
-   price: null
- };
- pageTitle = "Add Product";
- id: string;
- errorMessage;
-  constructor(private productService: ProductService, private router: Router, private route: ActivatedRoute) { }
+  product: Product;
+  pageTitle = "Add Product";
+  id: string;
+  errorMessage;
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    this.route.paramMap.subscribe(params => {
-      if(params.has('id')){
+    this.route.paramMap.subscribe((params) => {
+      if (params.has("id")) {
         this.pageTitle = "Edit Product";
-        this.id = params.get('id');
-        this.productService.get(this.id).valueChanges().subscribe(product => {
-          this.product = product;
-        });
+        this.id = params.get("id");
+        this.productService
+          .get(this.id)
+          .valueChanges()
+          .subscribe((product) => {
+            this.product = product;
+          });
       }
     });
   }
 
-  saveproduct(productForm){
+  saveproduct(productForm) {
     let product = productForm.value;
-    if(this.id){
+    if (this.id) {
       //update product
       this.productService.update(this.id, product);
-    }else{
+    } else {
       //create product
       this.productService.create(product);
     }
 
-    this.router.navigate(['/manage-products']);
+    this.router.navigate(["/manage-products"]);
   }
-
 }
